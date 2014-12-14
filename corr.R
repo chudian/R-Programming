e6638cb09ed3f433670b27101de3eb3dbb1440b7
chudian <dchu19426@gmail.com>
2014-12-13 21:17:08 -0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,25 +1,22 @@
 corr <- function(directory, threshold = 0) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+  ## 'directory' is a character vector of length 1 indicating the location of
+  ## the CSV files
   
-  ## 'threshold' is a numeric vector of length 1 indicating the
-  ## number of completely observed observations (on all
-  ## variables) required to compute the correlation between
-  ## nitrate and sulfate; the default is 0
+  ## 'threshold' is a numeric vector of length 1 indicating the number of
+  ## completely observed observations (on all variables) required to compute
+  ## the correlation between nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
   setwd("C:/Coursera/R Programming/specdata")
-  files <- list.files( path = directory )
-  
-  cr <- c() 
-  
-  for(f in 1:length(files)){
-    data <- read.csv( paste(directory, "/", files[f], sep=",", header=TRUE) )
-    data <- data[complete.cases(data),]
-    if ( nrow(data) > threshold ) {
-      cr <- c(cr, cor(data$sulfate, data$nitrate) ) # append corralations
-    }
+  df = complete(directory)
+  ids = df[df["nobs"] > threshold, ]$id
+  corrr = numeric()
+  for (i in ids) {
+    
+    newRead = read.csv(paste(directory, "/", formatC(i, width = 3, flag = "0"), 
+                             ".csv", sep = ""))
+    dff = newRead[complete.cases(newRead), ]
+    corrr = c(corrr, cor(dff$sulfate, dff$nitrate))
   }
-  
-  return( cr )
+  return(corrr)
 }
